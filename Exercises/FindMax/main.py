012ba6169d289a464e06c2bb2e5f6bdeec2cb17e
Samin Azhan <samin005.bracu@gmail.com>
2020-05-28 17:09:59 +0600

    Added FindMax package for calculating max number in a list.

---
+++ Exercises/FindMax/main.py
@@ -0,0 +1,5 @@
+# from Exercises.FindMax import utils  # imports the entire file
+from Exercises.FindMax.utils import find_max  # import only the find_max method of the utils module/file
+
+numbers = [1, 5, 3, 8, 7, -9]
+print(find_max(numbers))
